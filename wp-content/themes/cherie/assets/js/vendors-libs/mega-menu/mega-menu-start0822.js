650437ba92f1ae1a77c222c2230b0aeb012b2729
Charbel Azar <77696672+Charbel-Azar@users.noreply.github.com>
2023-12-19 00:40:08 +0200

    s

---
+++ wp-content/themes/cherie/assets/js/vendors-libs/mega-menu/mega-menu-start0822.js
@@ -0,0 +1,1 @@
+(function($){"use strict";var $window=$(window);var startMegaMenu=function(){$(".art-mega-menu").accessibleMegaMenu({uuidPrefix:"accessible-megamenu",menuClass:"nav-menu",topNavItemClass:"nav-item",panelClass:"sub-nav",panelGroupClass:"sub-nav-group",hoverClass:"hover",focusClass:"focus",openClass:"open"});};$('document').ready(function(){startMegaMenu();});})(jQuery);
